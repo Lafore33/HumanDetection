a39a1f16e7a9cf3e425c6ef48d2a9ce1cbe7a54c
Lafore15 <mkov2n1517@gmail.com>
2024-03-16 23:08:44 +0100

    refactored

--- dataset.py
+++ dataset.py
@@ -18,26 +18,23 @@ class ImageDataset(torch.utils.data.Dataset):
     def __init__(self, image_root, label_root, transform=None):
         self.image_root = image_root
         self.label_root = label_root
-        self.titles = [title for title in os.listdir(image_root)]
+        self.size = len(os.listdir(image_root))
         self.transform = transform
 
     def __len__(self):
-        return len(self.titles)
+        return self.size
 
     def __getitem__(self, index):
-        image_file = self.titles[index]
+        image_file = f"img{index}.jpg"
         image_path = os.path.join(self.image_root, image_file)
         img = Image.open(image_path)
         if self.transform is not None:
             img = self.transform(img)
 
-        label_file = self.titles[index][:-3] + "txt"
+        label_file = image_file[:-3] + "txt"
         label_path = os.path.join(self.label_root, label_file)
 
-        with open(label_path, 'r') as file:
-            label = file.read().strip()
-            label = label.split("\n")
-            label = [np.array(vector[2:].split(), dtype="float64") for vector in label]
+        label = np.loadtxt(fname=label_path, delimiter=" ", ndmin=2)
 
         return img, label
 
@@ -55,14 +52,6 @@ transforms = transforms.Compose([torchvision.transforms.Grayscale(num_output_cha
 
 train_images = ImageDataset(image_root=train_images_path, label_root=train_labels_path, transform=transforms)
 val_images = ImageDataset(image_root=val_images_path, label_root=val_labels_path, transform=transforms)
-
+print(train_images[0])
 train_loader = torch.utils.data.DataLoader(train_images, batch_size=batch_size, shuffle=True)
-model = Model()
-with torch.no_grad():
-    x, y = next(iter(train_loader))
-    print(f'x = {x}')
-    print(f'y_true = {y}')
-    print(f'y_pred = {model(x)}')
 
-# проверка коректности модели
-summary(model, (3, 448, 448), device="cpu")
