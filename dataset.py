46e9164f4150f23888d6fbc3781a8a1ceccdfbef
Lafore15 <mkov2n1517@gmail.com>
2024-03-17 22:01:35 +0100

    refactored

--- dataset.py
+++ dataset.py
@@ -5,10 +5,10 @@ import torchvision
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from PIL import Image
-from model import YOLO
-from torchsummary import summary
-from torch import nn
-import cv2
+# from model import YOLO
+# from torchsummary import summary
+# from torch import nn
+# import cv2
 
 CELLS_SPLIT = 7
 NUM_BOXES = 2
@@ -41,14 +41,22 @@ class ImageDataset(torch.utils.data.Dataset):
         label_path = os.path.join(self.label_root, label_file)
 
         label = torch.tensor(np.loadtxt(fname=label_path, delimiter=" ", ndmin=2))
+        # the intuition here is that each cell predicts NUM_BOXES, it's the parameter we set ourselves;
+        # one box has 5 parameters [probability of box, x, y, width, height] + num_of classes
         label_matrix = torch.zeros((self.cells_split, self.cells_split, self.num_boxes * 5 + self.num_classes))
+
+        # here we are running through all boxes for one image and deciding which cell it belongs
         for box in label:
             class_label, x, y, height, width = box
             class_label = int(class_label)
+            # here we get vertical and horizontal number of cells for one particular box
             hc, vc = int(self.cells_split * x), int(self.cells_split * y)
+            # coordinates with respect to cell
             x_cell, y_cell = self.cells_split * x - hc, self.cells_split * y - vc
             height_cell, width_cell = self.cells_split * height, self.cells_split * width
 
+            # check whether this box for this class is presented the problem here is that model can detect only one
+            # object in a cell, so if we already have object of some class in this cell, we won't add another one
             if label_matrix[vc, hc, self.num_classes] == 0:
                 label_matrix[vc, hc, self.num_classes] = 1
                 box_coordinates = torch.tensor([x_cell, y_cell, width_cell, height_cell])
@@ -72,4 +80,3 @@ transforms = transforms.Compose([torchvision.transforms.Grayscale(num_output_cha
 train_images = ImageDataset(image_root=train_images_path, label_root=train_labels_path, transform=transforms)
 val_images = ImageDataset(image_root=val_images_path, label_root=val_labels_path, transform=transforms)
 train_loader = torch.utils.data.DataLoader(train_images, batch_size=batch_size, shuffle=True)
-
--- model.py
+++ model.py
@@ -2,6 +2,9 @@ import torch
 from torch import nn
 
 # [out_channels, kernel_size, stride, padding]
+# tuple --> Convolutional Layer
+# "M" --> MaxPooling Layer
+# "List[tuple, tuple, int] --> Convolution(-s) which is repeated n times"
 params = [
     (64, 7, 2, 3),
     "M",
@@ -27,7 +30,8 @@ params = [
 class CNNBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel, stride=1, padding=0):
         super(CNNBlock, self).__init__()
-        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel, stride=stride, padding=padding)
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel,
+                              stride=stride, padding=padding)
         self.batch_norm = nn.BatchNorm2d(out_channels)
         self.leaky_relu = nn.LeakyReLU(0.1)
 
@@ -39,12 +43,12 @@ class CNNBlock(nn.Module):
 
 
 class YOLO(nn.Module):
-    def __init__(self, config, in_channels=3, cells_amount=7, num_boxes=2, num_classes=1):
+    def __init__(self, config, in_channels=3, cells_split=7, num_boxes=2, num_classes=1):
         super(YOLO, self).__init__()
         self.in_channels = in_channels
         self.architecture = config
         self.conv_block = self.create_conv_block(self.architecture)
-        self.fc_block = self.create_fc_block(cells_amount, num_boxes, num_classes)
+        self.fc_block = self.create_fc_block(cells_split, num_boxes, num_classes)
 
     def forward(self, x):
         x = self.conv_block(x)
@@ -72,11 +76,14 @@ class YOLO(nn.Module):
 
         return nn.Sequential(*layers)
 
-    def create_fc_block(self, cells_amount, num_boxes, num_classes):
+    def create_fc_block(self, cells_split, num_boxes, num_classes):
         return nn.Sequential(
             nn.Flatten(),
-            nn.Linear(1024 * cells_amount * cells_amount, 496),
+            nn.Linear(1024 * cells_split * cells_split, 496),
             nn.Dropout(0.5),
             nn.LeakyReLU(0.1),
-            nn.Linear(496, cells_amount * cells_amount * (num_classes + num_boxes * 5))
+            # the idea here is that one cell can predict several bboxes but for only one object,
+            # so one cell predicts (two bboxes of size 5 + num_classes) * (cell_split ^ 2)
+            # it's current limitation that it cannot predict bboxes for more than one object
+            nn.Linear(496, cells_split * cells_split * (num_classes + num_boxes * 5))
         )
